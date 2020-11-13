bb053d30a78a982ed19b4b135880eb129e7ec793
ullagulla <ulrikaa91@hotmail.com>
2020-11-13 15:47:46 +0100

    fixing smooth scroll to map

--- js/main.js
+++ js/main.js
@@ -1,26 +1,46 @@
 $(document).ready(function () {
 
+    //Ger subarea-info unika id:n
+    $(".subarea-info").each(function(i) {
+        $(this).attr("id", "subarea-info" + i);
+    });
+
+    //Hover på meny ger border-bottom klass
+
     $(".rubrik").hover(function() {
         $(this).toggleClass("clicked");
     })
 
-    $(".footer-links").hover(function() {
-        $(this).toggleClass("clicked");
-    })
+    //Loopar igenom kartan för att ge unikt id
 
     $(".subarea").each(function (i) {
+
+        //Vid hover på kartan, sänk opacitet
         $(this).attr("id", "subarea" + i).hover(function() {
             $("#subarea" + i).css("opacity", "0.4");
         }, function() {
             $("#subarea" + i).css("opacity", "1");
         })
+        //Vid klick på kartan, smooth scroll till vald sektion
+        $("#subarea" + i).click(function() {
+            // e.preventDefault()
+            // $("html, body").stop();
+            $('html, body').animate({
+                scrollTop: $("#subarea-info" + i).offset().top
+            }, 2000);
+        });
+        $(window).bind("mousewheel", function() {
+            $("html, body").stop();
+        });
 
+        //Vid mouseenter på län i listan, ändra färg och ge underline
         $("#subarea" + i).mouseenter(function() {
             $("#area_" + i).css({
                 "color": "rgba(0, 0, 0, 0.623)", 
                 "text-decoration": "underline"});
         })
 
+        //Vid mouseleave på län i listan, ändra färg och ta bort underline
         $("#subarea" + i).mouseleave(function() {
             $("#area_" + i).css({
                 "color": "black", 
@@ -28,6 +48,7 @@ $(document).ready(function () {
         })
     })
 
+    //Loopa igenom län-listan och ge styling på listan samt kartan vid hover
     $(".region_link_list").each(function(i) {
         $("#area_" + i).mouseenter(function() {
             $("#subarea" + i).css("opacity", "0.4"),
@@ -35,7 +56,7 @@ $(document).ready(function () {
                 "color": "rgba(0, 0, 0, 0.623)", 
                 "text-decoration": "underline"});
         })
-
+        //Ta bort styling vid mouse-leave
         $("#area_" + i).mouseleave(function() {
             $("#subarea" + i).css("opacity", "1");
             $("#area_" + i).css({
@@ -44,8 +65,19 @@ $(document).ready(function () {
         })
 
         $(".subarea-info").each(function(i) {
-            $(this).attr("id", "subarea-info" + i);
+            //Scroll till sektion vid klick på län-listan
+            $("#area_" + i).click(function( ) {
+                $("html, body").stop();
+                $('html, body').animate({
+                    scrollTop: $("#subarea-info" + i).offset().top
+                }, 2000);
+            });
+            $(window).bind("mousewheel", function() {
+                $("html, body").stop();
+            });
         });
+
+
     });
 
     $(window).scroll(function(){
